3ef945f18c2322be6d0e59c87b98516350cb19f2
mukremingokduman <mukremingokduman@gmail.com>
2023-06-25 17:50:45 +0300

    app icon & token

--- app/src/main/java/com/mukremingokduman/weatherappclone/service/WeatherApi.kt
+++ app/src/main/java/com/mukremingokduman/weatherappclone/service/WeatherApi.kt
@@ -6,10 +6,10 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
-    //https://api.openweathermap.org/data/2.5/weather?q=sakarya&appid=a3100d18e66fa7623ba5ed6eb1d2ed97
 
-   // @GET("data/2.5/weather?q=sakarya&appid=a3100d18e66fa7623ba5ed6eb1d2ed97")
-    @GET("data/2.5/weather?&units=metric&appid=a3100d18e66fa7623ba5ed6eb1d2ed97")
+
+   // @GET("data/2.5/weather?q=sakarya&appid=your_api_token")
+    @GET("data/2.5/weather?&units=metric&appid=your_api_token")
     fun getData(
         @Query("q") cityName: String
     ): Single<WeatherModel>
--- app/src/main/java/com/mukremingokduman/weatherappclone/service/WeatherApiService.kt
+++ app/src/main/java/com/mukremingokduman/weatherappclone/service/WeatherApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class WeatherApiService {
-    //https://api.openweathermap.org/data/2.5/weather?q=sakarya&appid=a3100d18e66fa7623ba5ed6eb1d2ed97
+
 
     private val BASE_URL = "https://api.openweathermap.org/"
     private val api = Retrofit
